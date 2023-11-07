031a7bc105f0fd98130fcd6d7281dcf7c8de667c
Mai Anh Nguyễn Thị <maianhnguyen234567@gmail.com>
2023-11-07 13:27:55 +0700

    Sửa delete

--- BaseSolution.BUS/DataTransferObjects/Services/Request/ServiceDeleteRequest.cs
+++ BaseSolution.BUS/DataTransferObjects/Services/Request/ServiceDeleteRequest.cs
@@ -10,6 +10,5 @@ namespace BaseSolution.Application.DataTransferObjects.Services.Request
     {
         public Guid Id { get; set; }
         public Guid? DeletedBy { get; set; }
-        public DateTimeOffset DeletedTime { get; set; }
     }
 }

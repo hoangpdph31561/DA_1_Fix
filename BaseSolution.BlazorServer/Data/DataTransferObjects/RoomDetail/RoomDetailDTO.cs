3d74a5d5218e2d360d7e0cdcbb1c3d0c47de1118
Nguyen Hong Hai <hainhph35304@fpt.edu.vn>
2023-11-22 21:43:47 +0700

    View RoomBookingDetail cho khách hàng, xóa extension tạo code

--- BaseSolution.BlazorServer/Data/DataTransferObjects/RoomDetail/RoomDetailDTO.cs
+++ BaseSolution.BlazorServer/Data/DataTransferObjects/RoomDetail/RoomDetailDTO.cs
@@ -1,14 +1,18 @@
-﻿namespace BaseSolution.BlazorServer.Data.DataTransferObjects.RoomDetail
+﻿using BaseSolution.BlazorServer.Data.ValueObjects.Common;
+
+namespace BaseSolution.BlazorServer.Data.DataTransferObjects.RoomDetail
 {
     public class RoomDetailDTO
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string NameOfRoomType { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public int MaxPeopleStay { get; set; }
         public string Description { get; set; } = string.Empty;
         public double RoomSize { get; set; }
         public List<string> Images { get; set; } = new List<string>();
+        public EntityStatus Status { get; set; }
         public Guid FloorId { get; set; }
         public Guid RoomTypeId { get; set; }
     }

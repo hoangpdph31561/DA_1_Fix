6b947c87525f4526f89c0dc9111180c61b5cdbff
hoangpdph31561 <hoangpdph31561@fpt.edu.vn>
2023-11-24 22:15:14 +0700

    sửa lỗi

--- BaseSolution.Infrastructure/Extensions/AutoMapperProfiles/RoomTypeProfile.cs
+++ BaseSolution.Infrastructure/Extensions/AutoMapperProfiles/RoomTypeProfile.cs
@@ -11,7 +11,7 @@ namespace BaseSolution.Infrastructure.Extensions.AutoMapperProfiles
         {
             CreateMap<RoomTypeEntity, RoomTypeDTO>()
                 .ForMember(desc => desc.NumberOfAmenityRoomDetail, opt => opt.MapFrom(src => src.AmenityRoomDetails.Count()))
-                .ForMember(desc => desc.AmountOfRoomType, opt => opt.MapFrom(src => src.RoomDetails.Count()));
+                .ForMember(desc => desc.AmountOfRoomType, opt => opt.MapFrom(src => src.RoomDetails.Count()))
                 .ForMember(dest => dest.NumberOfRoomDetails, opt => opt.MapFrom(src => src.RoomDetails.Count(rd => !rd.Deleted)))
                 .ForMember(dest => dest.MaxPriceOfRoom, opt => opt.MapFrom(src => src.RoomDetails == null || !src.RoomDetails.Any() ? 0 : src.RoomDetails.Max(rd => rd.Price)))
                 .ForMember(dest => dest.MinPriceOfRoom, opt => opt.MapFrom(src => src.RoomDetails == null || !src.RoomDetails.Any() ? 0 : src.RoomDetails.Min(rd => rd.Price)))

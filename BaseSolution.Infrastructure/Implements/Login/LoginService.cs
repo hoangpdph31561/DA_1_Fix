8ddd19fd34e2d766999e2c1572c7925bce071d0a
Mai Anh Nguyễn Thị <maianhnguyen234567@gmail.com>
2023-11-29 20:54:21 +0700

    BE login

---
+++ BaseSolution.Infrastructure/Implements/Login/LoginService.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using BaseSolution.Application.DataTransferObjects.Account;
+using BaseSolution.Application.DataTransferObjects.Account.request;
+using BaseSolution.Application.Interfaces.Login;
+using BaseSolution.Application.Interfaces.Services;
+using BaseSolution.Application.ValueObjects.Common;
+using BaseSolution.Application.ValueObjects.Response;
+using BaseSolution.Domain.Entities;
+using BaseSolution.Infrastructure.Database.AppDbContext;
+using BaseSolution.Infrastructure.Implements.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseSolution.Infrastructure.Implements.Login
+{
+    public class LoginService : ILoginService
+    {
+        private readonly AppReadOnlyDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly ILocalizationService _localizationService;
+
+        public LoginService(AppReadOnlyDbContext dbContext, IMapper mapper, ILocalizationService localizationService)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _localizationService = localizationService;
+        }
+        public async Task<RequestResult<ViewLoginInput>> Login(LoginInputRequest request)
+        {
+            // Bước 1 : lấy ra được User có user Name và password truyền xuống 
+            try
+            {
+                var result = new ViewLoginInput(); // khai báo để còn return 
+                var user = _dbContext.Users.FirstOrDefault(x => x.UserName == request.UserName && x.Password == request.Password);
+                if (user != null)
+                {
+                    // lấy ra Role của User đó
+                    result.UserName = user.UserName;
+                    result.Password = user.Password;
+                    var role = _dbContext.UserRoles.FirstOrDefault(x => x.Id == user.UserRoleId);
+                    if (role != null)
+                    {
+                        result.UserRoleId = role.Id;
+                    }
+                }
+                return RequestResult<ViewLoginInput>.Succeed(result);
+            }
+            catch (Exception e)
+            {
+                return RequestResult<ViewLoginInput>.Fail(_localizationService["Login fail"], new[]
+               {
+                    new ErrorItem
+                    {
+                        Error = e.Message,
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ BaseSolution.Infrastructure/ViewModels/Login/LoginViewModel.cs
@@ -0,0 +1,44 @@
+﻿using BaseSolution.Application.DataTransferObjects.Account.request;
+using BaseSolution.Application.Interfaces.Login;
+using BaseSolution.Application.Interfaces.Services;
+using BaseSolution.Application.ValueObjects.Common;
+using BaseSolution.Application.ViewModels;
+
+namespace BaseSolution.Infrastructure.ViewModels.Login
+{
+    public class LoginViewModel : ViewModelBase<LoginInputRequest>
+    {
+        private readonly ILoginService _loginService;
+        private readonly ILocalizationService _localizationService;
+
+        public LoginViewModel(ILoginService loginService , ILocalizationService localizationService)
+        {
+            _loginService = loginService;
+            _localizationService = localizationService;
+        }
+        public async override Task HandleAsync(LoginInputRequest data, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await _loginService.Login(data);
+                Data = result.Data!;
+                Success = result.Success;
+                ErrorItems = result.Errors;
+                Message = result.Message;
+                return;
+            }
+            catch (Exception)
+            {
+                Success = false;
+                ErrorItems = new[]
+                {
+                    new ErrorItem
+                    {
+                        Error = _localizationService["Login Fail"],
+                    }
+                };
+            }
+        
+        }
+    }
+}

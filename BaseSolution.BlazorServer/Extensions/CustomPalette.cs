055bc040752cab9d6d25e9554a4975f57d8fede6
hoangpdph31561 <hoangpdph31561@fpt.edu.vn>
2023-12-11 17:37:25 +0700

    color

--- BaseSolution.BlazorServer/Extensions/CustomPalette.cs
+++ BaseSolution.BlazorServer/Extensions/CustomPalette.cs
@@ -1,5 +1,7 @@
 ﻿namespace BaseSolution.BlazorServer.Extensions;
 public static class CustomPalette
 {
-    public static string MainColor { get; set; } = "#b54405";
+    public static string InfoColor { get; set; } = "#b54405";
+    public static string PrimaryColor { get; set; } = "#dd6700";
+
 }
